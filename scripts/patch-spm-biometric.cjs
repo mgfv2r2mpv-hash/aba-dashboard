6a9f2af634b86eb1eb024af067594637d03ebe9c
mgfv2r2mpv-hash <solus.son@gmail.com>
2026-06-21 16:18:53 -0400

    fix: remove stale conflict marker from patch-spm-biometric.cjs

--- scripts/patch-spm-biometric.cjs
+++ scripts/patch-spm-biometric.cjs
@@ -41,7 +41,6 @@ let package = Package(
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm")
             ],
-<<<<<<< HEAD
             path: "ios/Sources/BiometricAuthNative",
             sources: ["BiometricAuthNative.swift"])
     ]
